085bcc8e61e5cc9e31f5024e05ab72525dae66c9
gabeedlc <gabee.dlc@gmail.com>
2021-01-26 00:21:43 -0300

    initial commit

---
+++ coder-tienda-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Coder Tienda App</h1>
+        <p>
+          Entrega clase 02
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
