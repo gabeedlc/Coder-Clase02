31e53f40be59caa21a5d4fb59a4d0f31ef15963e
gabeedlc <gabee.dlc@gmail.com>
2021-01-28 00:13:26 -0300

    Update según consigna desafio Clase 03

--- coder-tienda-app/src/App.js
+++ coder-tienda-app/src/App.js
@@ -1,17 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { FaBeer } from 'react-icons/fa';
+import NavbarComponent from './components/navbar';
 
 function App() {
   return (
+    <>
+
     <div className="App">
+      <NavbarComponent />
       <header className="App-header">
         <h1>Coder Tienda App</h1>
         <p>
-          Entrega clase 02
+          Entrega clase 03
         </p>
+
         
       </header>
     </div>
+    </>
   );
 }
 
---
+++ coder-tienda-app/src/components/navbar/index.jsx
@@ -0,0 +1,55 @@
+import { SiBathasu } from "react-icons/si";
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/js/dist/dropdown.js'; 
+
+const NavbarComponent = () => {
+  
+    return (
+        <>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">Coder-Tienda <SiBathasu /> </a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+                    </li>
+                    <li className="nav-item dropdown">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Productos
+                        </a>
+                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a className="dropdown-item" href="#">Categoria 1</a></li>
+                            <li><a className="dropdown-item" href="#">Categoria 2</a></li>
+                            <li><a className="dropdown-item" href="#">Categoria 3</a></li>
+                            <li><a className="dropdown-item" href="#">Categoria 4</a></li>
+                            <li><hr className="dropdown-divider" /> </li>
+                            <li><a className="dropdown-item" href="#">Clase A</a></li>
+                            <li><a className="dropdown-item" href="#">Clase B</a></li>
+                            <li><a className="dropdown-item" href="#">Clase C</a></li>
+                        </ul>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contacto</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Acerca de</a>
+                    </li>
+                </ul>
+                <form className="d-flex">
+                    <input className="form-control me-2" type="search" placeholder="Ingrese su busqueda" aria-label="Search" />
+                    <button className="btn btn-outline-success" type="submit">Buscar</button>
+                </form>
+                </div>
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default NavbarComponent
